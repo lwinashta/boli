92f2ff0ef1c3e598e2086713e4386c82db47eb64
Pramod Ashta <pashta@nextgen.com>
2019-10-14 11:46:28 -0400

    v0.2 Mac

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ const express = require('express');
 const app = express();
 const port=8080;
 
+app.set('view engine', 'ejs');
+
 app.use('/resources',express.static('resources'));
 app.use('/node_modules',express.static('node_modules'));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/partials/index.html')); 
+    res.render('pages/index');
+    res.end();
 });
 
 app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/partials/login.html')); 
+    res.render('pages/login');
+    //res.sendFile(path.join(__dirname+'/partials/login.html')); 
 });
 
 app.listen(port,()=>console.log(`listening on port ${port}!`));
---
+++ resources/js/login.js
@@ -0,0 +1,8 @@
+// import {form} from '../modules/form';
+// const jq=require('jquery')
+
+// form.form=
+// form.submit();
+
+var person = require('../modules/form');
+console.log(person.firstName + ' ' + person.lastName);
